c9eab25deac663f2f6a8b881ac9d3935efcfc0a7
James Andricksen <james.andricksen@zopa.com>
2023-11-01 18:59:58 +0000

    Added google tag

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,12 +13,20 @@ class Home extends Component {
     return (
       <FullscreenImage image="/images/home.jpg">
         {/* <NavMenu menuList={this.props.menuList} musicParams={options} /> */}
-        <div className="ml-8 pt-8 w-1/3 font-sans">
-          <h1 className="mb-5 uppercase tracking-wide font-thin md:text-5xl text-4xl text-white">
+        <div className="ml-8 pt-8 w-1/3 font-mono">
+          <h1 className="mb-5 uppercase tracking-wide font-thin md:text-5xl text-4xl text-black">
             I Do Colour
           </h1>
-          <p className="tracking-wide font-hairline text-white md:text-2xl text-sm">
-            Colour consultancy by Tiffany Singh
+          <p className="tracking-wide font-hairline text-black md:text-2xl text-sm">
+            A mindful colour consultancy company
+          </p>
+          <br></br>
+          <p className="tracking-wide font-hairline text-black md:text-2xl text-sm">
+            Artist Website: <a href="https://www.tiffanysingh.com">www.tiffanysingh.com</a>
+          </p>
+          <br></br>
+          <p className="tracking-wide font-hairline text-black md:text-2xl text-sm">
+            Contact: info@idocolour.com
           </p>
         </div> 
         {/* <div className="items-stretch md:w-1/4 w-2/3 md:flex fixed md:pin-r pin-b m-20 justify-center">
