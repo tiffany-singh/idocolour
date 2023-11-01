42ec95044584d5cccfe25167daad591904cd7691
James Andricksen <james.andricksen@zopa.com>
2023-11-01 14:07:14 +0000

    Updates

---
+++ precache-manifest.3072f89d0b87203efdcca8798075fbe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4a7e0f868effd75224a",
+    "url": "/static/css/main.1da691c9.chunk.css"
+  },
+  {
+    "revision": "b4a7e0f868effd75224a",
+    "url": "/static/js/main.b4a7e0f8.chunk.js"
+  },
+  {
+    "revision": "a3259e0accb5d5da945d",
+    "url": "/static/js/1.a3259e0a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f01955d391a814d9c7646633e3847fb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/idocolour/precache-manifest.f6552e9b15b2ba600ced28f01d7f46b5.js"
+  "/precache-manifest.3072f89d0b87203efdcca8798075fbe9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/idocolour/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
