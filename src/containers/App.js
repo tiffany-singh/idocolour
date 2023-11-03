ebe759a607a1d4715b17c20427f18bb5a11a65e0
James Andricksen <james.andricksen@zopa.com>
2023-11-03 06:04:18 +0000

    Try browser router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { HashRouter,Switch, Route } from 'react-router-dom';
 import Projects from './Projects';
 import Commissions from './Commissions';
 import Contact from './Contact';
@@ -10,21 +10,13 @@ import EpkPage from './EpkPage';
 var data = require('../data/mockdata.json');
 
 const App = () => [
-    <Switch>
-        <Route exact path='/' component={() => <Home menuList={data.menu}/>}/>
-        <Route exact path='/about' component={() => <Projects menuList={data.menu}/>}/>
-        <Route exact path='/projects/fullcode' component={() => <StandardPage inputData={data.fullcode} menuList={data.menu} media={data.fullcode.media}/>}/>
-        <Route exact path='/projects/challengehaus' component={() => <StandardPage inputData={data.challenge_haus} menuList={data.menu} media={data.challenge_haus.media}/>}/>
-        <Route exact path='/projects/assembly' component={() => <StandardPage inputData={data.assembly} menuList={data.menu} media={data.assembly.media}/>}/>
-        <Route exact path='/projects/brainstare.manatorquim' component={() => <StandardPage inputData={data.brainstare_manatorquim} menuList={data.menu} media={data.brainstare_manatorquim.media}/>}/>
-        {/* <Route exact path='/commissions' component={() => <Commissions menuList={data.menu}/>}/> */}
-        <Route exact path='/commissions/totalinternalreflection' component={() => <StandardPage inputData={data.tir} menuList={data.menu} media={data.tir.media}/>}/>
-        <Route exact path='/commissions/beyondrefuge' component={() => <StandardPage inputData={data.beyond_refuge} menuList={data.menu} media={data.beyond_refuge.media}/>}/>
-        <Route exact path='/commissions/untitleddream' component={() => <StandardPage inputData={data.untitled_dream} menuList={data.menu} media={data.untitled_dream.media}/>}/>
-        <Route exact path='/commissions/returner' component={() => <StandardPage inputData={data.returner} menuList={data.menu} media={data.returner.media}/>}/>
-        <Route exact path='/contact' component={() => <Contact menuList={data.menu}/>}/>
-        <Route exact path='/epk' component={() => <EpkPage inputData={data.epk} menuList={data.menu} media={data.epk.media}/>}/>
-    </Switch>
+    <HashRouter>
+        <Switch>
+            <Route exact path='/' component={() => <Home menuList={data.menu}/>}/>
+            <Route exact path='/about' component={() => <Projects menuList={data.menu}/>}/>
+            <Route exact path='/contact' component={() => <Contact menuList={data.menu}/>}/>
+        </Switch>
+    </HashRouter>
 ]
 
 export default App;
